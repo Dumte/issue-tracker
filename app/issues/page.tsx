daf36ed2fd8a602290eaad966eb304e92a204faf
dumte <dummy4real@gmail.com>
2023-11-23 14:42:24 +0100

    Initialize the git command

---
+++ app/issues/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const IssuesPage = () => {
+    const currentPath = usePathname()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IssuesPage
